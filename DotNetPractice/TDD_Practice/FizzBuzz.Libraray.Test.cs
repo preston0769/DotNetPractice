aa436e7a3dd0897d62e2726ba9dab9fd4ae37423
Preston Zhang <preston.yuantao.zhang@gmail.com>
2017-03-08 21:08:17 +1100

    FizzBuzz_TDD
    First test and practice TDD using very simple FizzBuzz test code

---
+++ DotNetPractice/TDD_Practice/FizzBuzz.Libraray.Test.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice.TDD_Practice
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void WhenDefault_returnDefault([Values(1,2,4)]int input)
+        {
+            //Act
+            string output = FizzBuzzer.GetValue(input);
+
+            //Assertion
+            Assert.AreEqual(input.ToString(), output);
+        }
+        
+        [Test]
+        public void WhenDiv3_returnFizz([Values(3,6,9,12)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        public void WhenDiv5_returnBuzz([Values(5,10)]int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Buzz", output);
+
+        }
+
+        [Test]
+        public void WhenDiv15_returnFizzBuzz([Values(15,30,45)]int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("FizzBuzz", output);
+            
+        }
+        
+    }
+}
---
+++ DotNetPractice/TDD_Practice/FizzBuzz.Libraray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice.TDD_Practice 
+{
+    class FizzBuzzer
+    {
+        internal static string GetValue(int input)
+        {
+            if (input % 15 == 0)
+                return "FizzBuzz";
+            if (input % 3 ==0)
+                return "Fizz";
+            if (input % 5 == 0)
+                return "Buzz";
+            return input.ToString();
+        }
+    }
+}
